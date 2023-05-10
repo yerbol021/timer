e4d0ad75da59f7c9e7f19e5870c0e0a6884fd200
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-05-09 22:36:23 -0700

    making timer

---
+++ timer1.js
@@ -0,0 +1,28 @@
+// const timer = timer();
+// let time =[];
+// for (const element of timer){
+//   setTimeout(()=>{
+//     process.stdout.write('\x07 ${element}');
+//   }, time);
+// }
+
+// timer.js
+
+// Function to play beep sound
+function timer() {
+  process.stdout.write('\x07');
+}
+
+// Parse command line arguments
+const alarms = process.argv.slice(2).map(Number).sort((a, b) => a - b);
+
+// Set timeouts for each alarm
+alarms.forEach(alarm => {
+  // Make sure the alarm is a positive number
+  if (alarm > 0) {
+    setTimeout(() => {
+      timer();
+      console.log(`Alarm went off at ${alarm} seconds.`);
+    }, alarm * 1000);
+  }
+});
